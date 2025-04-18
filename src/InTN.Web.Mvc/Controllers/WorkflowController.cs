9ebe9a1d6887f99ec1dbbfddb70863b5adb9c6d1
NGUYEN THANH TU <thanhtuk9acntt@hotmail.com>
2025-04-18 15:35:25 +0700

    add view

---
+++ src/InTN.Web.Mvc/Controllers/WorkflowController.cs
@@ -0,0 +1,14 @@
+﻿using Abp.AspNetCore.Mvc.Authorization;
+using InTN.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InTN.Web.Controllers
+{
+    public class WorkflowController : InTNControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
