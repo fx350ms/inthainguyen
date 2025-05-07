bf4f32740b3d90c2674a7dacf4249a757a4e4cc4
NGUYEN THANH TU <thanhtuk9acntt@hotmail.com>
2025-05-07 15:19:19 +0700

    home page

---
+++ src/InTN.Application/StatisticAndReporting/Dto/StatisticReportNumberDto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InTN.StatisticAndReporting.Dto
+{
+    public class StatisticSummaryDto
+    {
+        /// <summary>
+        /// Tổng đơn hàng
+        /// </summary>
+        public int TotalOrders { get; set; }
+
+        /// <summary>
+        /// Tổng khách hàng
+        /// </summary>
+        public int TotalCustomers { get; set; }
+
+        /// <summary>
+        /// Tổng công nợ
+        /// </summary>
+        public decimal TotalDebt { get; set; }
+
+        /// <summary>
+        /// Tổng tiền hàng
+        /// </summary>
+        public decimal TotalOrderAmount { get; set; }
+
+        /// <summary>
+        /// Tổng giao dịch
+        /// </summary>
+        public int TotalTransaction { get; set; }
+    }
+
+    public class StatisticSummaryByDateDto
+    {
+        public DateTime Date { get; set; }
+        public int TotalOrders { get; set; }
+       // public int TotalCustomers { get; set; }
+        public decimal TotalDebt { get; set; }
+        public decimal TotalOrderAmount { get; set; }
+        public int TotalTransaction { get; set; }
+    }
+}
---
+++ src/InTN.Web.Mvc/Models/Home/HomePageModel.cs
@@ -0,0 +1,11 @@
+﻿using InTN.StatisticAndReporting.Dto;
+using System.Collections.Generic;
+
+namespace InTN.Web.Models.Home
+{
+    public class HomePageModel
+    {
+        public StatisticSummaryDto StatisticSummary { get; set; }
+        public List<StatisticSummaryByDateDto> StatisticSummaryByDate { get; set; }
+    }
+}
